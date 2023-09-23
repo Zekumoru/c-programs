5554182935a5af56c4f84c94cbd27f0313322c25
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-09-23 17:27:46 +0200

    Re-implement reallocation logic to n log n

--- dynamic-input/main.c
+++ dynamic-input/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char * getLineInput();
-int getStrLength(char[]);
 
 int main() {
     char * string;
@@ -11,27 +11,36 @@ int main() {
     string = getLineInput();
     
     printf("%s\n", string);
-    printf("The length of the string is %d\n", getStrLength(string));
+    printf("The length of the string is %d\n", (int) strlen(string));
 
     return 0;
 }
 
 char * getLineInput() {
     char ch;
-    int length = 2;
-    char* string = malloc(length);
+    int length = 1;
+    int allocLength = 2;
+    char * string = malloc(length);
     
     while ((ch = getchar()) != '\n' && ch != EOF) {
-        string[length - 2] = ch;
-        string = (char*) realloc(string, length);
         length++;
+        string[length - 2] = ch;
+
+        // Memory reallocation of O(n log n)
+        if (length >= allocLength) {
+            string = (char*) realloc(string, allocLength);
+            allocLength *= 2;
+        }
     }
 
-    return string;
-}
+    if (length == 1) {
+        return string;
+    }
 
-int getStrLength(char string[]) {
-    int length = 0;
-    while (string[length] != '\0') length++;
-    return length;
+    // Allocate a new char pointer string with the actual length
+    char * returnString = malloc(length);
+    memcpy(returnString, string, length);
+    free(string);
+
+    return returnString;
 }
