2a10115b033711c6129c38a93233e6e7af0cb316
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-09-25 01:02:54 +0200

    Move constants to constants.h

--- binary-conversions/lib/constants.h
+++ binary-conversions/lib/constants.h
@@ -1,6 +1,9 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+#define FALSE 0
+#define TRUE 1
+
 #define ASCII_STRING_SIZE 9
 
 #endif
--- binary-conversions/lib/utils.h
+++ binary-conversions/lib/utils.h
@@ -1,7 +1,5 @@
 #ifndef UTILS_H
 #define UTILS_H
-#define FALSE 0
-#define TRUE 1
 
 char * getLineInput();
 
