c7acc9c892f47d3f10b79f5c862cc30845705859
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2023-10-05 18:07:17 +0200

    Add comments to windows basics

--- ncurses/programs/03-windows-basics.c
+++ ncurses/programs/03-windows-basics.c
@@ -10,14 +10,22 @@ int program03() {
     startY = 5;
     startX = 7;
 
+    // create a new window for ncurses
     WINDOW* win = newwin(height, width, startY, startX);
+    // refresh main window to draw window
+    // (though it'll be invisible at this point)
     refresh();
 
+    // draw borders around the window
     box(win, 0, 0);
 
+    // prints the string below in the window
     mvwprintw(win, 1, 1, "This is my box.");
+
+    // Updates the screen to show win
     wrefresh(win);
 
+    // prints the string below in the main window
     mvprintw(height + startY + 3, 0, "Press any key to exit...");
     getch();
 
